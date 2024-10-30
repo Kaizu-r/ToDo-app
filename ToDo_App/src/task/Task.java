06986ea6793fb89c8feb506cdf386d9928c34b38
Kaizu-r <jljoloc@gmail.com>
2024-10-30 17:33:42 +0800

    Added pseudocodes and start Task Class

--- ToDo_App/src/task/Task.java
+++ ToDo_App/src/task/Task.java
@@ -1,5 +1,23 @@
 package task;
 
-public class Task {
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.util.ArrayList;
+import java.util.Scanner;
 
+
+public class Task {
+	public ArrayList<Byte> category = new ArrayList<Byte>();
+	public String name = "";
+	public String description = "";
+	public byte[] duration = new byte[3];
+	//declaration 
+	public LocalDate duedate;
+	public LocalTime dueTime;
+	
+	Task(ArrayList<Byte> category, String name, String description, byte[] duration, LocalDate duedate, LocalTime dueTine) {
+		
+	}
+	
+	
 }
